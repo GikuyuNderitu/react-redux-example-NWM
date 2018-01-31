9385c00ca216f9afc1d22c7d748720fbc899dbd9
Gikuyu Nderitu <gikyunderitu@gmail.com>
2018-01-30 19:21:17 -0500

    Added comments for app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import InputArea from './InputArea';
 import NumDisplay from './NumDisplay';
 import './App.css';
 
+/*
+  We've imported the default exported value from each file/package
+*/
+
+
+
+/* 
+  Notice what we are exporting in this file. 
+
+  We haven't given our function a name, we're just exporting the value of a function that returns the exported JSX elements (Remember, JSX is syntactic sugar for React.createElement)
+
+  Look at how we're using the components that we're importing from other files, Title, InputArea and Num Display. The app component is not explicitly passing them props
+*/
 export default function(props) {
   return (
     <div className="App">
